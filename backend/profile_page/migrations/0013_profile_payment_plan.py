b71109ee297401430f9827f46ccf5599b91783a1
Roman P <pythonpro@gmail.com>
2020-01-09 19:18:46 +0400

    Merge all task's migrations

--- backend/profile_page/migrations/0013_profile_payment_plan.py
+++ backend/profile_page/migrations/0013_profile_payment_plan.py
@@ -13,6 +13,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='profile',
             name='payment_plan',
-            field=models.PositiveSmallIntegerField(choices=[(1, 'Free'), (2, 'Standard'), (3, 'Enterprise')], default=1),
+            field=models.PositiveSmallIntegerField(choices=[(1, 'Free (1 node limit)'),
+                                                            (2, 'Standard (paid per node) with 1 month free trial'),
+                                                            (3, 'Enterprise')], default=1),
+        ),
+        migrations.AddField(
+            model_name='profile',
+            name='unlimited_customer',
+            field=models.BooleanField(default=False),
         ),
     ]
